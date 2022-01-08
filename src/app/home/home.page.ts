636eac8290928aa2fae5cd3ebb5d6c7bbdc010b4
Damir Arh <damir.arh@gmail.com>
2022-01-08 09:26:04 +0100

    Date picker with a reactive form validator

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  form: FormGroup;
 
-  constructor() {}
-
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      date: new FormControl(undefined, [Validators.required]),
+      text: new FormControl(undefined, [Validators.required]),
+    });
+  }
 }
